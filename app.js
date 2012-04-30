9485fe2bef9f5662d43841065d02f6b7866d2615
codereflection <jeff@codingreflection.com>
2012-04-29 17:21:17 -0700

    Getting friend loading from server working, and url for saving a new friend

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/friends', routes.friends);
+app.post('/friends', routes.saveFriends);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,20 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Friends List example using Node.js, express.js, jade, and Backbone' })
 };
+
+var friends = [
+          { name: 'Anita' },
+          { name: 'Jeff' },
+          { name: 'Jared' },
+          { name: 'Aidan' },
+          { name: 'Haven' },
+          ];
+
+exports.friends = function(req, res){
+  console.log('loading friends');
+  res.send(friends);
+};
+
+exports.saveFriends = function(req, res){
+  res.send('alrighty then, we have a new friend!');
+};
